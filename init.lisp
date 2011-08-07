d92a9f9bded1964eebb082943738062fdb115195
Eli Naeher <enaeher@gmail.com>
2011-08-07 10:01:43 -0500

    different way to handle init

--- init.lisp
+++ init.lisp
@@ -1,16 +1,10 @@
 (in-package :newshole)
 
-(eval-when (:compile-toplevel)
-  ;; Startup hooks
-  (pushnew (lambda ()
-             (with-open-file (stream ".newshole" :if-does-not-exist nil)
-               (when stream
-                 (let ((options (read stream :eof-error-p nil)))
-                   (setf *pomo-db-spec*
-                         (or (getf options :db-spec) *pomo-db-spec*))
-                   (setf *default-pathname-defaults*
-                         (or (getf options :default-pathname) *default-pathname-defaults*)))))
-             (start)
-             (sb-ext:quit))
-           sb-ext:*init-hooks*)
-  (pushnew #'swank:create-server sb-ext:*init-hooks*))
+(defun load-configuration ()
+  (with-open-file (stream ".newshole" :if-does-not-exist nil)
+    (when stream
+      (let ((options (read stream :eof-error-p nil)))
+        (setf *pomo-db-spec*
+              (or (getf options :db-spec) *pomo-db-spec*))
+        (setf *default-pathname-defaults*
+              (or (getf options :default-pathname) *default-pathname-defaults*))))))
--- start.lisp
+++ start.lisp
@@ -1,6 +1,7 @@
 (in-package :newshole)
 
 (defun start ()
+  (load-configuration)
   (apply #'pomo:connect-toplevel *pomo-db-spec*)
   (poll)
   (draw-all-charts))
